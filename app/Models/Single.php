9a74ca192a56afa9a16293fe27f82fc6629ebab2
Arararra <55007364+Arararra@users.noreply.github.com>
2025-09-23 13:21:23 +0700

    feat: add single model for single simple page

---
+++ app/Models/Single.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Single extends Model
+{
+    protected $fillable = [
+        'title',
+        'slug',
+        'image',
+        'body',
+        'accordions',
+    ];
+
+    protected $casts = [
+        'accordions' => 'array',
+    ];
+}
