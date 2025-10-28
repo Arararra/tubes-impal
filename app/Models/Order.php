d8121b44adf39f9a8b71de56cb575acc839b82d1
Arararra <55007364+Arararra@users.noreply.github.com>
2025-10-28 09:56:29 +0700

    feat: create essesntials model, migration, and resource

---
+++ app/Models/Order.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    protected $fillable = [
+        'customer_name', 
+        'customer_address', 
+        'customer_whatsapp',
+        'receipt', 
+        'shipping_receipt', 
+        'status', 
+        'total', 
+        'paid_date'
+    ];
+
+    protected $casts = [
+        'paid_date' => 'datetime',
+    ];
+
+    public function orderProducts()
+    {
+        return $this->hasMany(OrderProduct::class);
+    }
+
+    public function reviews()
+    {
+        return $this->hasMany(Review::class);
+    }
+}
