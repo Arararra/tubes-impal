dc6d5606b8a2adf8345e5c9097f87d6d3eacd148
Arararra <55007364+Arararra@users.noreply.github.com>
2025-10-29 18:52:11 +0700

    feat: create API for categories

--- routes/api.php
+++ routes/api.php
@@ -4,14 +4,14 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 
-use App\Models\User;
-
 use App\Http\Controllers\Api\SingleController;
+use App\Http\Controllers\Api\CategoryController;
 
 Route::middleware('auth.bearer')->group(function () {
-    Route::get('/test', function (Request $request) {
+    Route::get('/', function (Request $request) {
         return response()->json(['message' => 'Authorized']);
     });
 
     Route::apiResource('/singles', SingleController::class);
+    Route::apiResource('/categories', CategoryController::class);
 });
